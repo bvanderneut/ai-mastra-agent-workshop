f1158d61f52e7ad83f059a8cb72846133981fe6f
Bjorn van der Neut <bvanderneut@independer.nl>
2025-11-11 08:15:05 +0100

    Revert old function so other tools keep working

--- src/mastra/helpers.ts
+++ src/mastra/helpers.ts
@@ -11,3 +11,11 @@ export const parseAzureUrl = (url: string) => {
 
   return { apiBase, number: Number(number), type };
 };
+
+export const parseGitHubUrl = (url: string) => {
+  const match = url.match(/github.com\/(.+?)\/(.+?)\/(pull|issues)\/(\d+)/);
+  if (!match) throw new Error("Invalid GitHub URL");
+  const [_, owner, repo, type, number] = match;
+  const apiBase = `https://api.github.com/repos/${owner}/${repo}`;
+  return { apiBase, number: Number(number), type };
+};
